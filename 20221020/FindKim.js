a0710f407c4115a3dd5c8e7381ec29f221204784
hyeonmi3 <cc146@daum.net>
2022-10-20 10:29:17 +0900

    김서방 찾기

---
+++ 20221020/FindKim.js
@@ -0,0 +1,16 @@
+function solution(seoul) {
+    // let answer = '';
+
+    // for(let i = 0; i < seoul.length; i++) {
+    //     if(seoul[i] == "Kim") {
+    //         answer = i;
+    //         break;
+    //     }
+    // }
+
+    // return "김서방은 " + String(answer) + "에 있다";
+
+    let answer = seoul.indexOf('Kim');
+    return "김서방은 " + answer + "에 있다";
+    // 와 대박
+}
